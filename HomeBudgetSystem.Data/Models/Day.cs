f88b505bf878736c4725860d97cd5ab7bae4b182
Marian Dziubiak <marian.dziubiak@gmail.com>
2016-06-09 16:33:06 +0200

    Create basic models for daily expenses

---
+++ HomeBudgetSystem.Data/Models/Day.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeBudgetSystem.Data.Models
+{
+    public class Day
+    {
+        public DateTime Id { get; set; }
+        public List<Transaction> Expenses { get; set; }
+        public List<Transaction> Incomes { get; set; }
+    }
+}
---
+++ HomeBudgetSystem.Data/Models/Transaction.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeBudgetSystem.Data.Models
+{
+    public class Transaction
+    {
+        public string Title { get; set; }
+        public decimal Amount { get; set; }
+        public string Owner { get; set; }
+    }
+}
