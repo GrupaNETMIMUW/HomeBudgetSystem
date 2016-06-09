6ff4c5a39e4ce3b3ce041a43890d1cf14e29119c
Marian Dziubiak <marian.dziubiak@gmail.com>
2016-06-09 16:36:27 +0200

    Create a project with example data
    Create implementation of IDaysRepository

---
+++ HomeBudgetSystem.ExampleStorage/DaysRepository.cs
@@ -0,0 +1,35 @@
+﻿using HomeBudgetSystem.Data.Models;
+using HomeBudgetSystem.StorageAPI;
+using System;
+using System.Collections.Generic;
+
+namespace HomeBudgetSystem.ExampleStorage
+{
+    public class DaysRepository : IDaysRepository
+    {
+        public IEnumerable<Day> GetAllDays()
+        {
+            return new Day[0];
+        }
+        public Day GetDayByDate(DateTime date)
+        {
+            return new Day
+            {
+                Id = date,
+                Expenses = new List<Transaction>() {
+                    new Transaction { Owner = "Ja", Title = "Zakupy", Amount = 22.76M}
+                }
+            };
+        }
+
+        public void RemoveDay(Day day)
+        {
+
+        }
+
+        public void UpdateDay(Day day)
+        {
+
+        }
+    }
+}
