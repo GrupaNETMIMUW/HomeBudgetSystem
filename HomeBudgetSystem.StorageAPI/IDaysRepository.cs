bcd28d476616749c864b9af61b62f35295a45222
Marian Dziubiak <marian.dziubiak@gmail.com>
2016-06-09 16:35:59 +0200

    Create IDaysRepository

---
+++ HomeBudgetSystem.StorageAPI/IDaysRepository.cs
@@ -0,0 +1,16 @@
+﻿using HomeBudgetSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+
+namespace HomeBudgetSystem.StorageAPI
+{
+    public interface IDaysRepository
+    {
+        IEnumerable<Day> GetAllDays();
+        Day GetDayByDate(DateTime date);
+
+        void RemoveDay(Day day);
+
+        void UpdateDay(Day day);
+    }
+}
